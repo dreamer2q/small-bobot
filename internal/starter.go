c398d4fd37e64be11d9fc5b0891c0b91b9931c77
dreamer2q <dreamer22qq@gmail.com>
2020-11-25 11:55:31 +0800

    fix init

--- internal/starter.go
+++ internal/starter.go
@@ -1,13 +1,11 @@
 package internal
 
 import (
-	"miraigo-robot/config"
 	"miraigo-robot/internal/app"
 	"miraigo-robot/utils"
 )
 
 func Start() {
-	config.Init()
 	utils.WriteLogToFS()
 	app.Init()
 }
